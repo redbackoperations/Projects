510dde8ba5384ed7184e01acd6c0d11042d5c2b0
RGimolli <87452824+RGimolli@users.noreply.github.com>
2023-09-08 17:33:55 +1000

    Update MQTTManager.py

--- Project 1 - Tracking Players and Crowd Monitoring/DataScience/DataManager/MQTTManager.py
+++ Project 1 - Tracking Players and Crowd Monitoring/DataScience/DataManager/MQTTManager.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import paho.mqtt.client as mqtt
 import json
-import time 
+import time
 import ssl  # Importing ssl certificates - ensures data transmitted is encrypted
+from cryptography.fernet import Fernet #importing cryptogtaphy library
+
+# adding an encryption and decryption key
+encryption_key = Fernet.generate_key()
+cipher_suite = Fernet(encryption_key)
 
 class MQTTDataFrameHandler:
     def __init__(self, broker_address, topic, max_retries=3, retry_interval=5):
@@ -18,13 +23,15 @@ class MQTTDataFrameHandler:
     def _on_message(self, client, userdata, message):
         try:
             # Convert received message payload to DataFrame
+            encrypted data = message.payload # decrypt and convert received message
+            data_json = cipher_suite.decrypt(encrypted_data).decode('utf-8') # uses key to decrypt data
             data_json = message.payload.decode('utf-8')
             self.data = pd.read_json(data_json)
             # Add a timestamp column to the DataFrame
             self.data['timestamp'] = time.time()
         except Exception as e:
             self.error = str(e)
-
+            
     def receive_data(self, timeout=10):
         retries = 0
         while retries < self.max_retries:
@@ -53,8 +60,9 @@ class MQTTDataFrameHandler:
                 # Add a timestamp column before sending
                 df['timestamp'] = time.time()
                 json_data = df.to_json()
+                encrypted_data = cipher_suite.encrypt(json_data.encode('utf-8')) # Formats data to be encrypted
                 self.client.connect(self.broker_address, 1883, 60)
-                self.client.publish(self.topic, json_data)
+                sself.client.publish(self.topic, encrypted_data) # Converts into bytes
                 self.client.disconnect()
                 return
             except Exception as e:
@@ -62,22 +70,28 @@ class MQTTDataFrameHandler:
                 retries += 1
                 time.sleep(self.retry_interval)
         print("Max retries reached. Failed to send data.")
-
-def main():
-    # placeholders
+        
+    def main():
     broker_address = "YOUR_BROKER_ADDRESS"
     topic = "test/topic"
-
+    
     handler = MQTTDataFrameHandler(broker_address, topic)
-
+    
     # SSL setup for client
     handler.client.tls_set(ca_certs="ca.crt", certfile="client.crt", keyfile="client.key", tls_version=ssl.PROTOCOL_TLS) # Path should be rewritten to find actual files
     handler.client.username_pw_set("client_username", "client_password")  # 'client_username' and 'cliet_password' should be replaced with client's actual username and password
-
+    
     # Receiving data and converting it to DataFrame
     df_received = handler.receive_data()
     if df_received is not None:
         print(df_received)
+        
+        # Decrypts, processing the received data - if needed
+        decrypted_data_json = cipher_suite.decrypt(df_received.to_json().encode('utf-8')).decode('utf-8')
+        decrypted_df = pd.read_json(decrypted_data_json)
+        print("Decrypted DataFrame:")
+        print(decrypted_df)
+        
 
     # Sending DataFrame data through MQTT
     df_to_send = pd.DataFrame({
@@ -88,3 +102,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
