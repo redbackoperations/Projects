f0a89fa17f6911d3c1c4361187fe1588d65fd6bf
Aadi3333 <aaditya4838.be21@chitkara.edu.in>
2023-09-15 19:28:28 +1000

    Update GPS_HardwareSerial_Parsing.ino

--- Project 1 - Tracking Players and Crowd Monitoring/IoT Sensor Testing/GPS_HardwareSerial_Parsing/GPS_HardwareSerial_Parsing.ino
+++ Project 1 - Tracking Players and Crowd Monitoring/IoT Sensor Testing/GPS_HardwareSerial_Parsing/GPS_HardwareSerial_Parsing.ino
@@ -12,9 +12,9 @@ Adafruit_GPS GPS(&GPSSerial);
 
 uint32_t timer = millis();
 
+const String id = "1";
 
-void setup()
-{
+void setup() {
   //while (!Serial);  // uncomment to have the sketch wait until Serial is ready
 
   // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
@@ -31,7 +31,7 @@ void setup()
   // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
   // the parser doesn't care about other sentences at this time
   // Set the update rate
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);  // 1 Hz update rate
   // For the parsing code to work nicely and have time to sort thru the data, and
   // print it out we don't suggest using anything higher than 1 Hz
 
@@ -44,55 +44,63 @@ void setup()
   GPSSerial.println(PMTK_Q_RELEASE);
 }
 
-void loop() // run over and over again
+void loop()  // run over and over again
 {
+  //Serial.println(" Hello ");
   // read data from the GPS in the 'main loop'
   char c = GPS.read();
   // if you want to debug, this is a good time to do it!
-  if (GPSECHO)
-    if (c) Serial.print(c);
+  // if (GPSECHO)
+  //   if (c) Serial.print(c);
   // if a sentence is received, we can check the checksum, parse it...
   if (GPS.newNMEAreceived()) {
     // a tricky thing here is if we print the NMEA sentence, or data
     // we end up not listening and catching other sentences!
     // so be very wary if using OUTPUT_ALLDATA and trying to print out data
-    Serial.print(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
-    if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
-      return; // we can fail to parse a sentence in which case we should just wait for another
+    //Serial.print(GPS.lastNMEA());    // this also sets the newNMEAreceived() flag to false
+    if (!GPS.parse(GPS.lastNMEA()))  // this also sets the newNMEAreceived() flag to false
+      return;                        // we can fail to parse a sentence in which case we should just wait for another
   }
 
   // approximately every 2 seconds or so, print out the current stats
   if (millis() - timer > 2000) {
-    timer = millis(); // reset the timer
-    Serial.print("\nTime: ");
+    timer = millis();  // reset the timer
+    Serial.print(id + " ");
     if (GPS.hour < 10) { Serial.print('0'); }
-    Serial.print(GPS.hour, DEC); Serial.print(':');
+    Serial.print(GPS.hour, DEC);
+    Serial.print(':');
     if (GPS.minute < 10) { Serial.print('0'); }
-    Serial.print(GPS.minute, DEC); Serial.print(':');
+    Serial.print(GPS.minute, DEC);
+    Serial.print(':');
     if (GPS.seconds < 10) { Serial.print('0'); }
-    Serial.print(GPS.seconds, DEC); Serial.print('.');
-    if (GPS.milliseconds < 10) {
-      Serial.print("00");
-    } else if (GPS.milliseconds > 9 && GPS.milliseconds < 100) {
-      Serial.print("0");
-    }
-    Serial.println(GPS.milliseconds);
-    Serial.print("Date: ");
-    Serial.print(GPS.day, DEC); Serial.print('/');
-    Serial.print(GPS.month, DEC); Serial.print("/20");
-    Serial.println(GPS.year, DEC);
-    Serial.print("Fix: "); Serial.print((int)GPS.fix);
-    Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
+    Serial.print(GPS.seconds, DEC);
+
+    Serial.print(" ");
+
+    Serial.print(GPS.day, DEC);
+    Serial.print(':');
+    Serial.print(GPS.month, DEC);
+    Serial.print(":20");
+    Serial.print(GPS.year, DEC);
+
+    Serial.print(" ");
+
     if (GPS.fix) {
-      Serial.print("Location: ");
-      Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
-      Serial.print(", ");
-      Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
-      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
-      Serial.print("Angle: "); Serial.println(GPS.angle);
-      Serial.print("Altitude: "); Serial.println(GPS.altitude);
-      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
-      Serial.print("Antenna status: "); Serial.println((int)GPS.antenna);
+      Serial.print(GPS.latitude, 4);
+      Serial.print(GPS.lat);
+
+      Serial.print(" ");
+
+      Serial.print(GPS.longitude, 4);
+      Serial.print(GPS.lon);
+
+      Serial.print(" ");
+
+      Serial.print(GPS.altitude);
+
+      Serial.print(" ");
+
+      Serial.println((int)GPS.satellites);
     }
   }
 }
