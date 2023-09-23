f25242c1758b22cfc451f859826a0fe0271ce13f
MarsImmortal <s222198007@deakin.edu.au>
2023-09-23 12:02:37 +1000

    add routes for both player tracking and crowd monitoring

---
+++ Project 1 - Tracking Players and Crowd Monitoring/web_dev/api/routes/crowd.routes.js
@@ -0,0 +1,146 @@
+import express from 'express';
+import {Crowd} from '../models/crowd.model.js';
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const crowdData = await Crowd.find();
+    res.status(200).json(crowdData);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get('/heat-map', async (req, res) => {
+  try {
+    const crowdHeatMapData = await Crowd.find().select('heatMapData');
+    res.status(200).json(crowdHeatMapData);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get('/eventsOrIncidents', async (req, res) => {
+  try {
+    // Assuming events or incidents data is stored as a string in the database
+    const crowdEventsOrIncidents = await Crowd.find().select('eventsOrIncidents');
+    res.status(200).json(crowdEventsOrIncidents);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get('/trends', async (req, res) => {
+  try {
+    const crowdTrends = await Crowd.find().select('trends');
+    res.status(200).json(crowdTrends);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        if (
+            !req.body.timestamp ||
+            !req.body.location ||
+            !req.body.density ||
+            !req.body.heatMapData ||
+            !req.body.eventsOrIncidents ||
+            !req.body.trends
+        ) {
+            return res.status(400).send({ message: "Missing parameters" });
+        }
+      const CrowdData = {
+        timestamp: req.body.timestamp,
+        location: req.body.location,
+        density: req.body.density,
+        heatMapData: req.body.heatMapData,
+        eventsOrIncidents: req.body.eventsOrIncidents,
+        trends: req.body.trends,
+      };
+
+      const newCrowdData = await Crowd.create(CrowdData);
+  
+      res.status(201).json(newCrowdData);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+
+router.post('/heat-map', async (req, res) => {
+try {
+    if (!req.body.location || !req.body.heatMapData) {
+    return res.status(400).send({ message: 'Missing parameters (enter heat map data and location)' });
+    }
+
+    const crowdRecord = await Crowd.findOne({ location: req.body.location });
+
+    if (!crowdRecord) {
+    return res.status(404).json({ message: 'Crowd record not found' });
+    }
+
+
+    crowdRecord.heatMapData = req.body.heatMapData;
+
+
+    await crowdRecord.save();
+
+    res.status(200).json(crowdRecord);
+} catch (error) {
+    res.status(500).json({ message: error.message });
+}
+});
+
+router.post('/eventsOrIncidents', async (req, res) => {
+  try {
+    if (!req.body.location || !req.body.eventsOrIncidents) {
+      return res.status(400).send({ message: 'Missing parameters' });
+    }
+
+    const crowdRecord = await Crowd.findOne({ location: req.body.location });
+
+    if (!crowdRecord) {
+      return res.status(404).json({ message: 'Crowd record not found' });
+    }
+
+
+    crowdRecord.eventsOrIncidents = req.body.eventsOrIncidents;
+
+
+    await crowdRecord.save();
+
+    res.status(200).json(crowdRecord);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.post('/trends', async (req, res) => {
+  try {
+    if (!req.body.location || !req.body.trends) {
+      return res.status(400).send({ message: 'Missing parameters' });
+    }
+
+
+    const crowdRecord = await Crowd.findOne({ location: req.body.location });
+
+    if (!crowdRecord) {
+      return res.status(404).json({ message: 'Crowd record not found' });
+    }
+
+
+    crowdRecord.trends = req.body.trends;
+
+
+    await crowdRecord.save();
+
+    res.status(200).json(crowdRecord);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
+export default router;
---
+++ Project 1 - Tracking Players and Crowd Monitoring/web_dev/api/routes/playerRoutes.js
@@ -0,0 +1,251 @@
+import express from 'express';
+
+import {HeartRate} from '../models/heart.model.js';
+import {Player} from '../models/players.model.js';
+const router = express.Router();
+
+
+
+router.post('/', async (req, res) => {
+    try {
+        if (
+            !req.body.firstName ||
+            !req.body.lastName ||
+            !req.body.age ||
+            !req.body.team ||
+            !req.body.position ||
+            !req.body.dateOfBirth ||
+            !req.body.sport
+        ) {
+            return res.status(400).send({ message: "Missing parameters" });
+        }
+
+        const newPlayer = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            age: req.body.age,
+            team: req.body.team,
+            position: req.body.position,
+            dateOfBirth: req.body.dateOfBirth,
+            sport: req.body.sport,
+            heartRate: req.body.heartRate || null,
+            movements: req.body.movements || [], 
+            statistics: req.body.statistics || {},
+        };
+
+        const player = await Player.create(newPlayer);
+        return res.status(201).send(player);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err.message);
+    }
+});
+
+
+router.get('/', async (req, res) =>
+{
+    try {
+        const players = await Player.find({});
+        return res.status(200).json({count : players.length, data : players});
+    } catch (error) {
+        console.log(error);
+        res.send(500).json({message : error.message});
+    }
+}
+);
+
+router.get('/:id/statistics', async (req, res) => {
+    try {
+      const { id } = req.params;
+      const player = await Player.findById(id);
+  
+      if (!player) {
+        return res.status(404).json({ message: 'Player not found' });
+      }
+  
+      const playerStatistics = player.statistics; // Assuming 'statistics' is a field in the player document
+  
+      res.status(200).json(playerStatistics);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+
+  router.put('/:id/statistics', async (req, res) => {
+    const { id } = req.params;
+    const {
+      gamesPlayed,
+      goalsScored,
+      assists,
+      yellowCards,
+      redCards,
+    } = req.body;
+  
+    try {
+      const player = await Player.findById(id);
+  
+      if (!player) {
+        return res.status(404).json({ message: 'Player not found' });
+      }
+  
+      // Validate input data
+      if (typeof gamesPlayed !== 'number' || gamesPlayed < 0 ||
+          typeof goalsScored !== 'number' || goalsScored < 0 ||
+          typeof assists !== 'number' || assists < 0 ||
+          typeof yellowCards !== 'number' || yellowCards < 0 ||
+          typeof redCards !== 'number' || redCards < 0) {
+        return res.status(400).json({ message: 'Invalid input data' });
+      }
+
+      // Update player's statistics
+      player.statistics.gamesPlayed = gamesPlayed;
+      player.statistics.goalsScored = goalsScored;
+      player.statistics.assists = assists;
+      player.statistics.yellowCards = yellowCards;
+      player.statistics.redCards = redCards;
+  
+      await player.save();
+  
+      // Return the updated player object with statistics
+      res.status(200).json(player);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+});
+
+router.get('/:id/movements', async (req, res) => {
+    try {
+        const { id } = req.params;
+        
+        if (id.length !== 24) {
+            return res.status(400).json({ message: 'Invalid player id' });
+        }
+
+        const player = await Player.findById(id).select('movements');
+
+        if (!player) {
+            return res.status(404).json({ message: 'Player not found' });
+        }
+
+        return res.status(200).json({ movements: player.movements });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+router.post('/:id/movements', async (req, res) => {
+    try {
+        if (
+            !req.body.time ||
+            !req.body.date ||
+            !req.body.location ||
+            !req.body.alt ||
+            !req.body.sat
+        ) {
+            return res.status(400).send({ message: "Missing parameters" });
+        }
+
+        const { id } = req.params;
+        
+        if (id.length !== 24) {
+            return res.status(400).json({ message: 'Invalid player id' });
+        }
+
+        const player = await Player.findById(id);
+
+        if (!player) {
+            return res.status(404).json({ message: 'Player not found' });
+        }
+
+
+        const { time, date, location, alt, sat } = req.body;
+t
+        const newMovement = {
+            time,
+            date,
+            location,
+            alt,
+            sat,
+        };
+
+        player.movements.push(newMovement);
+
+        await player.save();
+
+        return res.status(201).json({ message: 'Movement data added successfully', movement: newMovement });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+router.get('/:id', async (req, res) =>
+{
+    try {
+        const {id} = req.params;
+        if (id.length != 24) {
+            return res.status(400).json({ message: 'Invalid player id' });
+        }
+        const player = await Player.findById(id);
+        if (!player) {
+            return res.status(404).send({ message: "Player not found" });
+        }
+        return res.status(200).json(player);
+    } catch (error) {
+        console.log(error);
+        res.send(500).json({message : error.message});
+    }
+}
+);
+
+router.delete('/:id', async (req, res) =>
+{
+    try {
+        const {id} = req.params;
+        if (id.length != 24) {
+            return res.status(400).json({ message: 'Invalid player id' });
+        }
+        const player = await Player.findByIdAndDelete(id);
+        if (!player) {
+            return res.status(404).send({ message: "Player not found" });
+        }
+        return res.status(200).json(player);
+    } catch (error) {
+        console.log(error);
+        res.send(500).json({message : error.message});
+    }
+}
+);
+
+router.put('/:id', async (req, res) => {
+    try {
+        if (
+            !req.body.firstName ||
+            !req.body.lastName ||
+            !req.body.age ||
+            !req.body.team ||
+            !req.body.position ||
+            !req.body.dateOfBirth ||
+            !req.body.sport
+        ) {
+            return res.status(400).send({ message: "Missing parameters" });
+        }
+        const {id} = req.params;
+
+        const result = await Player.findByIdAndUpdate(id , req.body);
+
+        if (!result) 
+        {
+            return res.status(404).json({message : 'player not found'});
+        }
+        return res.status(200).json({message : 'player updated successfully'}); 
+        }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({message : err.message});
+    }
+});
+
+export default router;
+
