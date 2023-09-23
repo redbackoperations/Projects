55eb2b91abc857bf03b0c30b5ce9e93642700c7b
MohitJurani <jurani.mohit@gmail.com>
2023-09-23 16:25:25 +1000

    Create Landing And Login

---
+++ Project 1 - Tracking Players and Crowd Monitoring/Mohit Jurani/src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Description } from "../Components/Description";
+import { Features } from "../Components/Features";
+
+export const Home = () => {
+  return (
+    <>
+      <Description />
+      <Features />
+    </>
+  );
+};
