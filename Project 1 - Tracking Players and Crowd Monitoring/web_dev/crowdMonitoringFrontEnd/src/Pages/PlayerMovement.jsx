c6d9b882689e08b271ee2aa6521e14b8aff0bb5f
MarsImmortal <s222198007@deakin.edu.au>
2023-09-24 17:06:25 +1000

    add pages for listing player, update palyer, player movement, and update player stats

--- Project 1 - Tracking Players and Crowd Monitoring/web_dev/crowdMonitoringFrontEnd/src/Pages/PlayerMovement.jsx
+++ Project 1 - Tracking Players and Crowd Monitoring/web_dev/crowdMonitoringFrontEnd/src/Pages/PlayerMovement.jsx
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
-import Plot from 'react-plotly.js';
+import * as d3 from 'd3';
+import L from 'leaflet';
 
 const PlayerMovement = () => {
     const [playerMovements, setPlayerMovements] = useState([]);
     const [loading, setLoading] = useState(false);
     const { id } = useParams();
+    const svgRef = useRef(null);
 
     useEffect(() => {
         setLoading(true);
@@ -24,22 +26,116 @@ const PlayerMovement = () => {
             });
     }, [id]);
 
-    // Data for the line chart
-    const lineChart = {
-        x: playerMovements.map((movement) => movement.date),
-        y: playerMovements.map((movement) => movement.alt),
-        type: 'scatter',
-        mode: 'lines+markers',
-        name: 'Altitude',
-    };
+    useEffect(() => {
+      if (playerMovements.length > 0) {
+          createLineChart();
+          createBarChart();
+          createMap();
+      }
+  }, [playerMovements]);
 
-    // Data for the bar chart
-    const barChart = {
-        x: playerMovements.map((movement) => movement.date),
-        y: playerMovements.map((movement) => movement.sat),
-        type: 'bar',
-        name: 'Satellite Count',
-    };
+  const createLineChart = () => {
+    const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+    const width = 400 - margin.left - margin.right;
+    const height = 300 - margin.top - margin.bottom;
+
+    const svg = d3.select(svgRef.current);
+
+    svg.selectAll('*').remove(); // Clear the existing SVG content
+
+    const parseDate = d3.timeParse('%Y-%m-%d %H:%M:%S'); // Format may vary, adjust as needed
+
+    const x = d3.scaleTime()
+        .domain(d3.extent(playerMovements, (d) => parseDate(d.date + ' ' + d.time)))
+        .range([0, width]);
+
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(playerMovements, (d) => d.alt)])
+        .nice()
+        .range([height, 0]);
+
+    const line = d3.line()
+        .x((d) => x(parseDate(d.date + ' ' + d.time)))
+        .y((d) => y(d.alt));
+
+    svg.append('g')
+        .attr('transform', `translate(${margin.left},${margin.top})`)
+        .call(d3.axisLeft(y));
+
+    svg.append('g')
+        .attr('transform', `translate(${margin.left}, ${height + margin.top})`)
+        .call(d3.axisBottom(x));
+
+    svg.append('path')
+        .datum(playerMovements)
+        .attr('fill', 'none')
+        .attr('stroke', 'blue')
+        .attr('stroke-width', 2)
+        .attr('d', line);
+};
+
+const createBarChart = () => {
+    const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+    const width = 400 - margin.left - margin.right;
+    const height = 300 - margin.top - margin.bottom;
+
+    const svg = d3.select(svgRef.current);
+
+    const x = d3.scaleBand()
+        .domain(playerMovements.map((d) => d.date))
+        .range([margin.left, width + margin.left])
+        .padding(0.1);
+
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(playerMovements, (d) => d.sat)])
+        .nice()
+        .range([height, margin.top]);
+
+    svg.selectAll('*').remove(); // Clear the existing SVG content
+
+    svg.selectAll('rect')
+        .data(playerMovements)
+        .enter().append('rect')
+        .attr('x', (d) => x(d.date))
+        .attr('y', (d) => y(d.sat))
+        .attr('width', x.bandwidth())
+        .attr('height', (d) => height - y(d.sat))
+        .attr('fill', 'steelblue');
+    
+    svg.append('g')
+        .attr('transform', `translate(0,${height})`)
+        .call(d3.axisBottom(x));
+
+    svg.append('g')
+        .attr('transform', `translate(${margin.left}, 0)`)
+        .call(d3.axisLeft(y));
+};
+
+const createMap = () => {
+  const width = 400;
+  const height = 300;
+
+  // Create a Leaflet map
+  const map = L.map(svgRef.current).setView([0, 0], 2); // Set the initial view coordinates and zoom level
+
+  // Add a map tile layer (you can use different tile sources)
+  L.tileLayer('http://tile.stamen.com/watercolor/{z}/{x}/{y}.jpg').addTo(map);
+
+  // Plot player locations as markers on the map
+  playerMovements.forEach((movement) => {
+      const lat = parseFloat(movement.location.lat);
+      const lon = parseFloat(movement.location.log);
+
+      if (!isNaN(lat) && !isNaN(lon)) {
+          L.marker([lat, lon]).addTo(map);
+      }
+  });
+
+  // Set the map's dimensions
+  map.invalidateSize();
+  map.setMaxBounds(map.getBounds());
+};
+
 
     return (
         <div className='p-4'>
@@ -77,28 +173,10 @@ const PlayerMovement = () => {
                     </div>
                     <div>
                         <h2>Altitude Over Time</h2>
-                        <Plot
-                            data={[lineChart]}
-                            layout={{
-                                width: 400,
-                                height: 300,
-                                title: 'Altitude vs. Date',
-                                xaxis: { title: 'Date' },
-                                yaxis: { title: 'Altitude' },
-                            }}
-                        />
+                        <svg ref={svgRef} width="400" height="300"></svg>
 
                         <h2>Satellite Count Over Time</h2>
-                        <Plot
-                            data={[barChart]}
-                            layout={{
-                                width: 400,
-                                height: 300,
-                                title: 'Satellite Count vs. Date',
-                                xaxis: { title: 'Date' },
-                                yaxis: { title: 'Satellite Count' },
-                            }}
-                        />
+                        <svg ref={svgRef} width="400" height="300"></svg>
                     </div>
                 </div>
             )}
