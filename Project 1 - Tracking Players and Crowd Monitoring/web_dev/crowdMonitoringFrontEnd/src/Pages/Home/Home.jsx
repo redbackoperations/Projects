37141c349ef34dfd05cff3f5e1a4e84cb78b45b6
MarsImmortal <s222198007@deakin.edu.au>
2023-09-24 17:12:12 +1000

    modify the code uploaded by mohit to integrate with backend and vite app make some functions and change structure

---
+++ Project 1 - Tracking Players and Crowd Monitoring/web_dev/crowdMonitoringFrontEnd/src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Description  from "../../components/Description";
+import Features  from "../../components/Features";
+
+const Home = () => {
+  return (
+    <>
+      <Description />
+      <Features />
+    </>
+  );
+};
+
+export default Home;
