cc77c25946fe7adb19c5174cb591cdceae7a16d7
AsselMulla <Aseel.w.mulla@gmail.com>
2023-11-15 19:02:10 +1100

    Building admin page, and adding users to Django admin page

--- Project 8 - Backend/backend_server/models.py
+++ Project 8 - Backend/backend_server/models.py
@@ -1,5 +1,5 @@
 from django.db import models 
 
 class Users(models.Model):
-    name= models.CharField(max_length=20)
+    username= models.CharField(max_length=20)
     password = models.CharField(max_length=20)
