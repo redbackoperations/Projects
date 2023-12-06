11c43a3bd5c243bd8f332890de8c65974aa589a0
s222569052 <150748532+s222569052@users.noreply.github.com>
2023-12-06 15:34:28 +1100

    Deleting

--- Project 1 - Tracking Players and Crowd Monitoring/web_dev/crowdMonitoringFrontEnd/src/Pages/Crowd/CrowdHM.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CrowdHM = () => {
-  return (
-    <div>CrowdHM</div>
-  )
-}
-
-export default CrowdHM
--- Project 1 - Tracking Players and Crowd Monitoring/web_dev/crowdMonitoringFrontEnd/src/Pages/Crowd/CrowdsTrends.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CrowdsTrends = () => {
-  return (
-    <div>CrowdsTrends</div>
-  )
-}
-
-export default CrowdsTrends
